74b9bf62c1cf2f11e6d3be1c4cc1995da4317163
Kingdom-Coding <jonathanv2283219@gmail.com>
2025-02-01 17:45:19 -0500

    initialization

---
+++ server.js
@@ -0,0 +1,40 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+http
+  .createServer((request, response) => {
+    let addr = request.url,
+      q = new URL(addr, "http://" + request.headers.host);
+    filePath = "";
+
+    fs.appendFile(
+      "log.txt",
+      "URL: " + addr + "\nTimeStamp: " + new Date() + "\n\n",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Added to log.");
+        }
+      }
+    );
+
+    if (q.pathname.includes("documentation")) {
+      filePath = __dirname + "/documentation.html";
+    } else {
+      filePath = "index.html";
+    }
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      response.writeHead(200, { "content-Type": "text/html" });
+      response.write(data);
+      response.end();
+    });
+  })
+  .listen(5800);
+console.log("my test server is running in port 8080");
